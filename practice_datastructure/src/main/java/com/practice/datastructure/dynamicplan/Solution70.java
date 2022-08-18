8cf164cb9654e553d9ff973eeeea735b51e93056
Maoyongjie <1871770518@qq.com>
2022-08-18 19:41:25 +0800

    20220818 动态规划 5、70

---
+++ practice_datastructure/src/main/java/com/practice/datastructure/dynamicplan/Solution70.java
@@ -0,0 +1,62 @@
+package com.practice.datastructure.dynamicplan;
+
+/**
+ * @author MaoYongjie
+ * @date 2022/8/18 16:36
+ * @Description:
+ */
+
+//假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+//
+// 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：n = 2
+//输出：2
+//解释：有两种方法可以爬到楼顶。
+//1. 1 阶 + 1 阶
+//2. 2 阶
+//
+// 示例 2：
+//
+//
+//输入：n = 3
+//输出：3
+//解释：有三种方法可以爬到楼顶。
+//1. 1 阶 + 1 阶 + 1 阶
+//2. 1 阶 + 2 阶
+//3. 2 阶 + 1 阶
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= n <= 45
+//
+// Related Topics 记忆化搜索 数学 动态规划
+// 👍 2588 👎 0
+public class Solution70 {
+    public static void main(String[] args) {
+        System.out.println((new Solution70()).climbStairs(3));
+    }
+    public int climbStairs(int n) {
+        int[] arr = new int[n + 1];
+
+        if (n >= 1) {
+            arr[1] = 1;
+        }
+        if (n >= 2) {
+            arr[2] = 2;
+        }
+        for (int i = 3; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[n];
+    }
+}
