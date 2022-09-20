1f76b1becabc4cf041a716edae7f1409380346e1
Maoyongjie <1871770518@qq.com>
2022-09-20 18:29:53 +0800

    20220915 排序

--- practice_datastructure/src/main/java/com/practice/datastructure/dynamicplan/Solution651FourKey.java
+++ practice_datastructure/src/main/java/com/practice/datastructure/dynamicplan/Solution651FourKey.java
@@ -1,4 +1,16 @@
 package com.practice.datastructure.dynamicplan;
 
+import com.alibaba.fastjson.JSON;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Solution651FourKey {
+    public static void main(String[] args) {
+//        List<S>
+        String str = "{}";
+        HashMap<String,Object> list = JSON.parseObject(str, HashMap.class);
+        System.out.println(list);
+    }
 }
